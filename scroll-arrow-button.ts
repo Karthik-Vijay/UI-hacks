d7661c38f14880a30ff0de1304d7005189090f4b
Karthik Vijay <55907622+Karthik-Vijay@users.noreply.github.com>
2022-06-07 00:53:35 +0530

    Update scroll-arrow-button.ts

--- scroll-arrow-button.ts
+++ scroll-arrow-button.ts
@@ -1,3 +1,17 @@
-  scrollDown() {
+import { ViewportScroller } from "@angular/common";  
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+export class HomeComponent implements OnInit {
+  
+ constructor( private scroller: ViewportScroller) { }
+  
+ scrollDown() {
    this.scroller.scrollToAnchor("targetScroll");
   }
+  
+}
